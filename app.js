5eb44c7b15c01f6403a0e10e51b7a4064da276ef
Michael Yang <Haofreshbne@gmail.com>
2025-06-26 00:12:08 +1000

    调整司机GIF标记尺寸为60x60像素
    - Google Maps: 司机标记从80x80调整到60x60像素
    - Leaflet: 司机标记从80x80调整到60x60像素
    - 调整锚点位置为中心点(30,30)
    - 调整脉冲动画范围为15px半径，更协调
    - 调整阴影效果为3px，与尺寸成比例
    - 实现适中的尺寸平衡：既醒目又不会过于突兀
    - 优化地图上司机位置的视觉效果

--- app.js
+++ app.js
@@ -519,8 +519,8 @@ async function displayDriverMap(orderId, driverData, deliveryAddress) {
     
     // 创建司机标记 (使用动态GIF图)
     const driverIcon = L.divIcon({
-        html: '<img src="./driver.gif" style="width: 80px; height: 80px; border-radius: 50%; border: 3px solid white;">',
-        iconSize: [80, 80],
+        html: '<img src="./driver.gif" style="width: 60px; height: 60px; border-radius: 50%; border: 3px solid white;">',
+        iconSize: [60, 60],
         className: 'driver-marker'
     });
     
@@ -599,8 +599,8 @@ async function displayGoogleMap(orderId, driverData, deliveryAddress, mapContain
         title: driverData.driver_name || '配送司机',
         icon: {
             url: './driver.gif',  // 使用新的司机GIF动画
-            scaledSize: new google.maps.Size(80, 80),  // 增大一倍尺寸以显示动画效果
-            anchor: new google.maps.Point(40, 40)
+            scaledSize: new google.maps.Size(60, 60),  // 调整为60x60像素，适中尺寸
+            anchor: new google.maps.Point(30, 30)
         }
     });
     
